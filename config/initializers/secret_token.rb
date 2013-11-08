60c42e4e1103b9e29cccf798b93c726ab049b3d7
SG <sy1840@gmail.com>
2013-11-08 01:46:09 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scottsportfolio::Application.config.secret_key_base = '3c7efe7987ca8e2cbb54ba6cc5d66fb19fa913f45f8b3ee61f870fa3db4672b41708fdc6d082d5503f03ed0a394b9547b0e2564e8eeeaefab0cedbb0e3146980'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scottsportfolio::Application.config.session_store :cookie_store, key: '_scottsportfolio_session'
