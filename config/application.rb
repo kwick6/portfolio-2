9656c8db96e62c82db7dcd040be82201036e339a
SG <sy1840@gmail.com>
2013-11-08 11:06:59 -0800

    move config code

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
+module Scottsportfolio
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
                                   "fontawesome-webfont.ttf",
                                  "fontawesome-webfont.eot",
                                  "fontawesome-webfont.svg",
@@ -14,27 +19,21 @@ config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
 
 
 
-config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
+    config.assets.precompile << Proc.new do |path|
+          if path =~ /\.(css|js)\z/
+            full_path = Rails.application.assets.resolve(path).to_path
+            app_assets_path = Rails.root.join('app', 'assets').to_path
+            if full_path.starts_with? app_assets_path
+              puts "including asset: " + full_path
+              true
+            else
+              puts "excluding asset: " + full_path
+              false
+            end
+          else
+            false
+          end
         end
-      else
-        false
-      end
-    end
-
-module Scottsportfolio
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
